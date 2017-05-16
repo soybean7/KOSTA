80c088b4c7cf7a2ea87b9946e6c32ca015855254
kosta <kosta@DESKTOP-PUU267R>
2017-05-16 11:05:21 +0900

    죄민상의 생각

--- FinalProjet_v0.02/src/onoffmix/controller/mobile/UserController.java
+++ FinalProjet_v0.02/src/onoffmix/controller/mobile/UserController.java
@@ -5,4 +5,22 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class UserController {
 	
+	private String MCguilty;
+	private String Usom;
+	private String hansomeKang;
+	private String wisdomLee;
+	private String emotinalWords;
+	
+	public String loveLine(){
+		String result;
+		
+		result = MCguilty +" feels fear when he tell " + Usom;
+		
+		return result;
+	}
+	
+	public void printScreen(){
+		System.out.println(loveLine());
+	}
+	
 }
