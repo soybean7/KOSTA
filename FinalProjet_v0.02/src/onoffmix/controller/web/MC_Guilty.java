227f18a7a881e9a9fef8ba96cff7f3c000e19777
soybean7 <aya9108@naver.com>
2017-05-22 17:40:07 +0900

    aa

--- FinalProjet_v0.02/src/onoffmix/controller/web/MC_Guilty.java
+++ FinalProjet_v0.02/src/onoffmix/controller/web/MC_Guilty.java
@@ -3,11 +3,28 @@ package onoffmix.controller.web;
 public class MC_Guilty {
 	
 	public void createFunctions(){
+<<<<<<< HEAD
+		// 죄민상
+		System.out.println("시스템이 생성되었습니다!");
+		
+		// 김승희
+		System.out.println("죄민상은 존잘러");
+	}
+
+	
+	public void deleteFunction(){
+		// 죄민상
+		System.out.println("시스템이 제거되었습니다!");
+		
+		// 김승희
+		System.out.println("Don't afraid her");
+=======
 		System.out.println("시스템이 생성되었습니다!");
 	}
 
 	
 	public void deleteFunction(){
 		System.out.println("시스템이 제거되었습니다!");
+>>>>>>> branch 'master' of https://github.com/soybean7/KOSTA
 	}
 }
