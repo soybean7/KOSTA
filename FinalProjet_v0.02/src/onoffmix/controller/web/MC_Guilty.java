42ee7d76789ccf2a10b21ff56a868215bb2e9406
soybean7 <aya9108@naver.com>
2017-05-22 17:19:08 +0900

    haha

--- FinalProjet_v0.02/src/onoffmix/controller/web/MC_Guilty.java
+++ FinalProjet_v0.02/src/onoffmix/controller/web/MC_Guilty.java
@@ -3,11 +3,11 @@ package onoffmix.controller.web;
 public class MC_Guilty {
 	
 	public void createFunctions(){
-		System.out.println("시스템이 생성되었습니다");
+		System.out.println("시스템이 생성되었습니다!");
 	}
 
 	
 	public void deleteFunction(){
-		System.out.println("시스템이 제거되었습니다");
+		System.out.println("시스템이 제거되었습니다!");
 	}
 }
