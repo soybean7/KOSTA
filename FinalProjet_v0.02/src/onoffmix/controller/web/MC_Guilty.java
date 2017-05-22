74961b7cbbfc0beac3413685f603fc5942244bf0
kosta <kosta@DESKTOP-PUU267R>
2017-05-22 17:23:26 +0900

    김승희 수정내용

--- FinalProjet_v0.02/src/onoffmix/controller/web/MC_Guilty.java
+++ FinalProjet_v0.02/src/onoffmix/controller/web/MC_Guilty.java
@@ -3,11 +3,13 @@ package onoffmix.controller.web;
 public class MC_Guilty {
 	
 	public void createFunctions(){
-		
+		System.out.println("죄민상은 존잘러");
 	}
 
 	
 	public void deleteFunction(){
 		
+		
+		System.out.println("Don't afraid her");
 	}
 }
