fce57d7d58ff63553210601ec9703ba044ec8c37
kosta <kosta@DESKTOP-PUU267R>
2017-05-22 17:27:07 +0900

    Merged source code

--- FinalProjet_v0.02/src/onoffmix/controller/web/MC_Guilty.java
+++ FinalProjet_v0.02/src/onoffmix/controller/web/MC_Guilty.java
@@ -3,13 +3,19 @@ package onoffmix.controller.web;
 public class MC_Guilty {
 	
 	public void createFunctions(){
+		// 죄민상
+		System.out.println("시스템이 생성되었습니다!");
+		
+		// 김승희
 		System.out.println("죄민상은 존잘러");
 	}
 
 	
 	public void deleteFunction(){
+		// 죄민상
+		System.out.println("시스템이 제거되었습니다!");
 		
-		
+		// 김승희
 		System.out.println("Don't afraid her");
 	}
 }
