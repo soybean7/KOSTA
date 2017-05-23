38117ee696f1a6bf4bb6c999b958c764511192a3
kosta <kosta@DESKTOP-GLAHMM4>
2017-05-23 16:12:33 +0900

    ㅇㅅㅇ

--- FinalProjet_v0.02/src/moigo/service/UserService.java
+++ FinalProjet_v0.02/src/moigo/service/UserService.java
@@ -7,11 +7,11 @@ import moigo.domain.User;
 //written SHKim at 20170517
 public interface UserService {
 	
-	void registUser(User user);
+	boolean registUser(User user);
 	int modifyUser(User user);
 	boolean removeUser(String userEmail);
 	User searchUser(String userEmail);
 	List<User> searchUserByName(String name);
 	List<User> searchUserByEmail(String userEmail);
-
+	User login(User user);
 }
--- FinalProjet_v0.02/src/moigo/store/logic/UserStoreLogic.java
+++ FinalProjet_v0.02/src/moigo/store/logic/UserStoreLogic.java
@@ -2,42 +2,53 @@ package moigo.store.logic;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import moigo.domain.User;
 import moigo.store.UserStore;
+import moigo.store.mybatis.mapper.UserMapper;
 
 @Repository
 public class UserStoreLogic implements UserStore {
+	
+	@Autowired
+	private UserMapper mapper;
 
 	@Override
-	public void insertUser(User user) {
-		
+	public boolean insertUser(User user) {
+		boolean insert = mapper.insertUser(user);
+		return insert;
 	}
 
 	@Override
 	public int updateUser(User user) {
-		return 0;
+		int update = mapper.updateUser(user);
+		return update;
 	}
 
 	@Override
 	public boolean deleteUser(String userEmail) {
-		return false;
+		boolean delete = mapper.deleteUser(userEmail);
+		return delete;
 	}
 
 	@Override
 	public List<User> selectUserByEmail(String userEmail) {
-		return null;
+		List<User> users = mapper.selectUserByEmail(userEmail);
+		return users;
 	}
 
 	@Override
 	public List<User> selectUserByName(String name) {
-		return null;
+		List<User> users = mapper.selectUserByName(name);
+		return users;
 	}
 
 	@Override
 	public User selectUser(String userEmail) {
-		return null;
+		User user = mapper.selectUser(userEmail);
+		return user;
 	}
 	
 	
