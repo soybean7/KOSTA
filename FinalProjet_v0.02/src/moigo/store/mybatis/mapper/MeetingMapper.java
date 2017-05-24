378d40e81622d24ce79c1309c1dfc1824e4523a8
kosta <kosta@DESKTOP-HP5UFGL>
2017-05-24 14:10:49 +0900

    jih

--- FinalProjet_v0.02/src/moigo/store/mybatis/mapper/MeetingMapper.java
+++ FinalProjet_v0.02/src/moigo/store/mybatis/mapper/MeetingMapper.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package moigo.store.mybatis.mapper;
 
 import java.sql.Date;
@@ -38,44 +37,3 @@ public interface MeetingMapper {
 
 	void deleteRequestMeeting(HashMap<String, Object> map);
 }
-=======
-package moigo.store.mybatis.mapper;
-
-import java.sql.Date;
-import java.util.HashMap;
-import java.util.List;
-
-import moigo.domain.Meeting;
-
-public interface MeetingMapper {
-	int insertMeeting(Meeting meeting);
-
-	int updateMeeting(Meeting meeting);
-
-	int deleteMeeting(int meetingId);
-
-	List<Meeting> selectAllMeeting();
-
-	Meeting selectMeetingById(int meetingId);
-
-	List<Meeting> selectMeetingByTitle(String title);
-
-	List<Meeting> selectMeetingByContent(String content);
-
-	List<Meeting> selectMeetingByDate(Date date);
-
-	List<Meeting> selectMeetingByPlace(String place);
-
-	List<Meeting> selectMeetingByCategory(String category);
-
-	List<Meeting> selectMeetingByHashtag(String hashtag);
-
-	List<Meeting> selectMeetingByReqeust(String email);
-
-	List<Meeting> selectMeetingByDone(String email);
-
-	void registRequestMeeting(HashMap<String, Object> map);
-
-	void deleteRequestMeeting(HashMap<String, Object> map);
-}
->>>>>>> branch 'master' of https://github.com/soybean7/KOSTA
