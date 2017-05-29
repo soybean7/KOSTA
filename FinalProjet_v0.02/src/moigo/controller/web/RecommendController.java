0e5f44c69eab731e496919b6051774a344f0dc6b
happykosta <kosta@kosta-HP>
2017-05-29 14:58:19 +0900

    RecommendController(수정)

--- FinalProjet_v0.02/src/moigo/controller/web/RecommendController.java
+++ FinalProjet_v0.02/src/moigo/controller/web/RecommendController.java
@@ -50,8 +50,40 @@ public class RecommendController {
 		return "recommend/recommendList";
 	}
 	
-	public String searchById(int meetingId, Model model){
-		return null;
+	@RequestMapping(value="detail.do")
+	public String searchById(int meetingId, Model model, HttpSession session){
+
+//		------------------------------------------------------------------------------------------------
+//		유저정보 임의 작성(삭제해야함)
+		session.setAttribute("userEmail", "somiTest");
+//		------------------------------------------------------------------------------------------------		
+		
+//		제안모임 정보 가져오기
+		Meeting meeting = service.searchMeetingById(meetingId);
+		model.addAttribute("meeting", meeting);
+
+		String userEmail = (String)session.getAttribute("userEmail");
+		if(userEmail != null){
+//			로그인 했을 경우
+			
+//			유저의 찜 정보 가져오기
+			List<String> users = service.searchLoveUserById(meetingId);
+			for(String user : users){
+				if(user.equals(userEmail)){
+					model.addAttribute("exist", "exist");
+				}
+			}
+		}else{
+//			로그인 안 했을 경우
+			System.out.println("로그인 필요");
+//			------------------------------------------------------------------------------------------------
+//			로그인 페이지로 이동(수정필요)
+			return "user/login.do";
+//			------------------------------------------------------------------------------------------------			
+		}
+
+		
+		return "recommend/recommendDetail";
 	}
 	
 	public String searchByName(String name, Model model){
@@ -70,8 +102,40 @@ public class RecommendController {
 		return null;
 	}
 	
-	public String loveRecommend(int meetingId, Model model){
-		return null;
+	@RequestMapping(value="love.do")
+	public String loveRecommend(int meetingId, Model model, HttpSession session){
+		
+		System.out.println("love.do");
+
+//		------------------------------------------------------------------------------------------------
+//		유저정보 임의 작성(삭제해야함)
+		session.setAttribute("userEmail", "somiTest");
+//		------------------------------------------------------------------------------------------------
+		
+		String userEmail = (String)session.getAttribute("userEmail");
+		
+		Meeting meeting = service.searchMeetingById(meetingId);
+		int loveCount = meeting.getSympathy();
+		
+		if(service.loveMeeting(meetingId, userEmail)){
+//			찜추가
+//			모임 추천수 +1
+			meeting.setSympathy(loveCount+1);
+//			------------------------------------------------------------------------------------------------			
+//			모임 업데이트
+//			------------------------------------------------------------------------------------------------			
+		}else {
+//			이미 찜 되어있던 경우
+//			찜 삭제
+			service.hateMeeting(meetingId, userEmail);
+//			모임 추천수 -1
+			meeting.setSympathy(loveCount-1);
+//			------------------------------------------------------------------------------------------------			
+//			모임 업데이트
+//			------------------------------------------------------------------------------------------------
+		}
+
+		return "redirect:/recommend/detail.do?meetingId="+meetingId;
 	}
 	
 	public String selectLoveRecommendBySort(String sortType, Model model){
